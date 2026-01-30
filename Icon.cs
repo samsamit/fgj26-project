a4875911699b6b078c08de50951895e762db364b
visma-samu-tiainen <samu.tiainen@visma.com>
2026-01-30 17:22:02 +0200

    ide setup and test game

---
+++ Icon.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class Icon : Sprite2D
+{
+	[Export]
+	public float Speed = 200.0f;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector2 velocity = Vector2.Zero;
+
+		if (Input.IsActionPressed("ui_right"))
+			velocity.X += 1;
+		if (Input.IsActionPressed("ui_left"))
+			velocity.X -= 1;
+		if (Input.IsActionPressed("ui_down"))
+			velocity.Y += 1;
+		if (Input.IsActionPressed("ui_up"))
+			velocity.Y -= 1;
+
+		if (velocity.Length() > 0)
+			velocity = velocity.Normalized() * Speed;
+
+		Position += velocity * (float)delta;
+	}
+}
