3c9758ca743124fe03102266a826ba93e85df196
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2026-01-30 23:15:07 +0200

    Basic signal handlig

--- scripts/GlobalStateManager.cs
+++ scripts/GlobalStateManager.cs
@@ -1,14 +1,18 @@
 using Godot;
-using System;
 using System.Collections.Generic;
 
-public class GlobalStateManager
+public partial class GlobalStateManager : Node
 {
     public static HashSet<string> CompletedPuzzle = [];
 
     public static Vector2 PlayerPosition = Vector2.Zero;
     public static Vector2 MaskPosition = Vector2.Zero;
 
+    public override void _Ready()
+    {
+        PuzzleCompleted += puzzleName => CompletedPuzzle.Add(puzzleName);
+    }
+
     [Signal]
-    public delegate void LevelCompletedEventHandler();
+    public delegate void PuzzleCompletedEventHandler(string puzzleName);
 }
