7b8fd259d5a4ad4ce65365e6519bf92929904a7b
CaptainCluster <sranta.ville@gmail.com>
2026-02-01 00:41:09 +0200

    Signal stuff

--- scenes/CompletionConditions/PressurePlateDown.cs
+++ scenes/CompletionConditions/PressurePlateDown.cs
@@ -4,25 +4,23 @@ namespace FGJ26LPR.scenes.CompletionConditions;
 
 public partial class PressurePlateDown : CompletionCondition
 {
-    [Export] private PressurePlate _pressurePlate;
+	[Export] private PressurePlate _pressurePlate;
 
-    private bool _isPressed = false;
-    
-    public override void _Ready()
-    {
-        _pressurePlate.Pressed += OnPressed;
-        _pressurePlate.Released += OnReleased;
-    }
+	private bool _isPressed = false;
+	
+	public override void _Ready()
+	{
+		_pressurePlate.Pressed += OnPressed;
+		_pressurePlate.Released += OnReleased;
+	}
 
-    private void OnPressed()
-    {
-        _isPressed = true;
-        MarkCompleted();
-    }
-    
-    private void OnReleased()
-    {
-        _isPressed = false;
-        MarkUncompleted();
-    }
+	private void OnPressed()
+	{
+		_isPressed = true;
+	}
+	
+	private void OnReleased()
+	{
+		_isPressed = false;
+	}
 }
