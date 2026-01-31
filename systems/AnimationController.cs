7980b52bd6d809e65ce2e509d688923bf0bba0c2
johan1954 <tomi.enberg@gmail.com>
2026-01-31 23:19:29 +0200

    Clean up animation controller

--- systems/AnimationController.cs
+++ systems/AnimationController.cs
@@ -1,32 +1,18 @@
 using Godot;
 using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-// This controller doesn't care what animations should be played. The parent calling the functions should.
-
 
 public partial class AnimationController : Node2D
 {
 	public AnimationEnum ActiveAnimation { get; private set; }
 	private AnimatedSprite2D _animatedSprite2D;
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		ActiveAnimation = AnimationEnum.Idle;
-		//_animatedSprite2D.AnimationChanged += WhenAnimationChanged;
-	}
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-
 	}
 
 	public void InitiateSpriteFrames(SpriteFrames spriteFrames, float scale)
 	{
-		//_animatedSprite2D = new AnimatedSprite2D();
-		//AddChild(_animatedSprite2D);
 		Scale = new Vector2(scale, scale);
 		_animatedSprite2D.SpriteFrames = spriteFrames;
 	}
@@ -38,7 +24,6 @@ public partial class AnimationController : Node2D
 			return;
 		}
 
-		GD.Print("Applying: " + animationEnum);
 		switch (animationEnum)
 		{
 			case AnimationEnum.Idle:
@@ -58,7 +43,7 @@ public partial class AnimationController : Node2D
 				_animatedSprite2D.Play(nameof(AnimationEnum.Push));
 				break;
 			default:
-				Console.WriteLine("Animation Enum not defined");
+				GD.PrintErr("Animation Enum not defined");
 				break;
 		}
 
@@ -83,7 +68,7 @@ public partial class AnimationController : Node2D
 		var wantedRotation = direction.Angle();
 		var currentRotation = Rotation;
 		var neededRotation = wantedRotation - currentRotation;
-		
+
 		if (neededRotation > Math.PI)
 		{
 			wantedRotation -= (float)(2 * Math.PI);
