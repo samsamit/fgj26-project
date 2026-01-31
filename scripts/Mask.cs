f3e08f710098e5f70713295cd1393fb35a1984f9
Roni Juntunen <18495705+softgitron@users.noreply.github.com>
2026-01-31 14:20:55 +0200

    Add support for mask changing

--- scripts/Mask.cs
+++ scripts/Mask.cs
@@ -5,9 +5,24 @@ public partial class Mask : Node2D
 {
 	public const float Speed = 200.0f;
 
+	[Export]
+	public Texture2D Round;
+
+	[Export]
+	public Texture2D Square;
+
+	[Export]
+	public Texture2D Star;
+
+	[Export]
+	public Texture2D Triangle;
+
+	private PointLight2D Light;
+
 	public override void _Ready()
 	{
 		GD.Print("Player script is active!");
+		Light = (PointLight2D)GetNode("./Light");
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -22,15 +37,26 @@ public partial class Mask : Node2D
 			Vector2 direction = (mousePosition - GlobalPosition).Normalized();
 			velocity = direction * Speed;
 		}
-
-		//Velocity = velocity;
-		//MoveAndSlide();
 	}
 
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
 		GlobalPosition = GetGlobalMousePosition();
+		//SetMask(MaskEnum.Square, 1, new Color("red"));
 	}
 
+	public void SetMask(MaskEnum mask, float maskSize, Color maskColor)
+	{
+		Light.TextureScale = maskSize;
+		Light.Color = maskColor;
+		Light.Texture = mask switch
+		{
+			MaskEnum.Round => Round,
+			MaskEnum.Square => Square,
+			MaskEnum.Star => Star,
+			MaskEnum.Triangle => Triangle,
+			_ => Round,
+		};
+	}
 }
