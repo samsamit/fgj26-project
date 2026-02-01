f2840c82b83647e9b879289d5db8ab6ca74d9196
Roni Juntunen <18495705+softgitron@users.noreply.github.com>
2026-02-01 11:18:46 +0200

    Add all musics

---
+++ scripts/BackgroundMusic.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class BackgroundMusic : AudioStreamPlayer
+{
+    [Export]
+    public AudioStream MainTheme;
+
+    [Export]
+    public AudioStream Drums;
+
+    [Export]
+    public AudioStream WithoutDrums;
+
+    public override void _Ready()
+    {
+        GlobalStateManager.Instance.AvailableMasks.RegisterObserver(UpdateMusic);
+    }
+
+    private void UpdateMusic(List<MaskEnum> masks)
+    {
+        if (masks.Contains(MaskEnum.XRay))
+        {
+            Stream = MainTheme;
+        }
+        else if (masks.Contains(MaskEnum.Strength))
+        {
+            Stream = Drums;
+        }
+        else
+        {
+            Stream = WithoutDrums;
+        }
+        Play();
+    }
+
+    public void OnFinished()
+    {
+        Play();
+    }
+}
