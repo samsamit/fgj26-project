bc413349136064f073a65252b33681937a1bb522
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2026-02-01 13:24:24 +0200

    Fix all problems

--- scripts/Projectile.cs
+++ scripts/Projectile.cs
@@ -6,14 +6,17 @@ public partial class Projectile : Area2D
     [Export] public SpeedComponent SpeedComponent;
     [Export] public AnimatableBody2D Body;
 
+    private VisibleOnScreenNotifier2D _screenNotifier;
+
     public override void _Ready()
     {
         BodyEntered += OnPlayerHit;
-        var screenNotifier = GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
-        screenNotifier.ScreenExited += QueueFree;
+        _screenNotifier = GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
+        _screenNotifier.ScreenExited += QueueFree;
     }
     public override void _PhysicsProcess(double delta)
     {
+        if (!_screenNotifier.IsOnScreen()) QueueFree();
         Position += new Vector2((float)(SpeedComponent.CurrentSpeed * delta), 0).Rotated(Rotation);
 
         if (Body.MoveAndCollide(Vector2.Zero) is not null)
