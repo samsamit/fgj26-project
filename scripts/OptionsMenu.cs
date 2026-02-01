bc545bc19f81131211224cdad4a895a5298f9fe3
AK-uni-git <aku.kauria@student.lut.fi>
2026-02-01 13:11:16 +0200

    Options menu slider value fix

--- scripts/OptionsMenu.cs
+++ scripts/OptionsMenu.cs
@@ -3,6 +3,21 @@ using System;
 
 public partial class OptionsMenu : Control
 {
+	private VBoxContainer _mainMenuButtons;
+
+	private Slider musicSlider, sfxSlider;
+
+	public override void _Ready()
+	{
+		_mainMenuButtons = GetNode<VBoxContainer>("../ButtonContainer");
+		_mainMenuButtons.Hide();
+
+		musicSlider = GetNode<Slider>("BackgroundMusic");
+		sfxSlider = GetNode<Slider>("SFX");
+
+		musicSlider.SetValue(AudioServer.GetBusVolumeLinear(AudioServer.GetBusIndex("Music")));
+		sfxSlider.SetValue(AudioServer.GetBusVolumeLinear(AudioServer.GetBusIndex("Effects")));
+	}
 	public void OnMusicVolumeChanged(float value)
 	{
 		var music = AudioServer.GetBusIndex("Music");
@@ -18,5 +33,6 @@ public partial class OptionsMenu : Control
 	public void OnBackButton()
 	{
 		QueueFree();
+		_mainMenuButtons.Show();
 	}
 }
