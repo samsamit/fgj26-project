d4bd4a9e7ca6e169f46e713ee7f321417eaafe37
johan1954 <tomi.enberg@gmail.com>
2026-02-01 11:28:47 +0200

    Main menu updates

--- scripts/MainMenuScript.cs
+++ scripts/MainMenuScript.cs
@@ -3,28 +3,76 @@ using System;
 
 public partial class MainMenuScript : Control
 {
+    private TextureRect _staticTexture;
+    private TextureRect _animatedTexture;
+    private Timer _timerToStartFlicker;
+    private Timer _timerToStopFlicker;
+    [Export] public float AnimationCreditsSpeed = 10.0f;
+    [Export] public float DefaultFlickerSpeed = 8.0f;
+    [Export] public int MinTimeBetweenFlickers = 5;
+    [Export] public int MaxTimeBetweenFlickers = 10;
+
     public override void _Ready()
+    {
+        _staticTexture = GetNode<TextureRect>("StaticTexture");
+        _animatedTexture = GetNode<TextureRect>("AnimatedTexture");
+        _timerToStartFlicker = GetNode<Timer>("FlickerStartTimer");
+        _timerToStopFlicker = GetNode<Timer>("FlickerTimeout");
+        _timerToStartFlicker.Start();
+        _animatedTexture.Visible = false;
+
+    }
+
+    public override void _Process(double delta)
     {
 
 
-        return;
     }
 
-    public void OnButtonStartPressed(){
+
+
+    public void OnButtonStartPressed()
+    {
         GetTree().ChangeSceneToFile("res://scenes/BaseScene.tscn");
-        GD.Print("Start pressed");
+        //GD.Print("Start pressed");
     }
 
-    public void OnButtonOptionsPressed(){
+    public void OnButtonOptionsPressed()
+    {
         GD.Print("Options pressed");
     }
 
-    public void OnButtonCreditsPressed(){
+    public void OnButtonCreditsPressed()
+    {
         GD.Print("Credits pressed");
     }
 
-    public void OnButtonExitPressed(){
+    public void OnButtonExitPressed()
+    {
         GetTree().Quit();
         //GD.Print("Exit pressed");
+    }
+
+    public void OnTimerTimeout()
+    {
+        _timerToStartFlicker.Stop();
+        _animatedTexture.Visible = true;
+        _staticTexture.Visible = false;
+        _timerToStopFlicker.Start();
+    }
+
+    public void OnFlickerComplete()
+    {
+        _timerToStopFlicker.Stop();
+        _animatedTexture.Visible = false;
+        _staticTexture.Visible = true;
+        _timerToStartFlicker.WaitTime = new Random().Next(MinTimeBetweenFlickers, MaxTimeBetweenFlickers);
+        GD.Print(_timerToStartFlicker.WaitTime);
+        _timerToStartFlicker.Start();
+    }
+
+    public void OnAnimatedTextureVisibilityChanged()
+    {
+        
     }
 }
