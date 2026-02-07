a79ac2950581f6a0245b4bf61838561b3ec75855
Lauri Tahvanainen <48516187+LauriTahvanainen@users.noreply.github.com>
2026-02-08 00:40:47 +0200

    Trigger different music in different stages

--- scripts/BackgroundMusic.cs
+++ scripts/BackgroundMusic.cs
@@ -15,16 +15,26 @@ public partial class BackgroundMusic : AudioStreamPlayer
 
 	public override void _Ready()
 	{
-		GlobalStateManager.Instance.AvailableMasks.RegisterAfterChangeObserver(UpdateMusic);
+		GlobalStateManager.Instance.AvailableMasks.RegisterAfterChangeObserver(UpdateMusicAfterAddingNewMask);
+		GlobalStateManager.Instance.PuzzleCompleted += ChangeMusicOnPuzzleCompletion;
 	}
 
-	private void UpdateMusic(List<MaskEnum> masks)
+	public override void _ExitTree()
+	{
+		GlobalStateManager.Instance.PuzzleCompleted -= ChangeMusicOnPuzzleCompletion;
+	}
+
+	private void UpdateMusicAfterAddingNewMask(List<MaskEnum> masks)
 	{
 		if (masks.Contains(MaskEnum.XRay))
 		{
-			// Stream = MainTheme;
+			Stream = WithoutDrums;
 		}
 		else if (masks.Contains(MaskEnum.Strength))
+		{
+			Stream = Drums;
+		}
+		else if (masks.Contains(MaskEnum.Slow))
 		{
 			Stream = Drums;
 		}
@@ -32,7 +42,35 @@ public partial class BackgroundMusic : AudioStreamPlayer
 		{
 			Stream = WithoutDrums;
 		}
-		Play();
+
+		FadeOutInMusic();
+	}
+
+	private void ChangeMusicOnPuzzleCompletion(string puzzleId)
+	{
+		if (puzzleId == nameof(Puzzle5))
+		{
+			Stream = MainTheme;
+			FadeOutInMusic();
+		}
+		else if (puzzleId == nameof(Puzzle4))
+		{
+			Stream = WithoutDrums;
+			FadeOutInMusic();
+		}
+	}
+
+	private void FadeOutInMusic()
+	{
+		// Fade out current music using tween and fade in the new one: short 1000ms crossfade;
+		var tween = CreateTween();
+		tween.TweenProperty(this, "volume_db", -80.0f, 1.0f)
+			 .SetTrans(Tween.TransitionType.Linear)
+			 .SetEase(Tween.EaseType.InOut);
+		tween.TweenCallback(Callable.From(() => Play()));
+		tween.TweenProperty(this, "volume_db", 0.0f, 1.0f)
+			 .SetTrans(Tween.TransitionType.Linear)
+			 .SetEase(Tween.EaseType.InOut);
 	}
 
 	public void OnFinished()
